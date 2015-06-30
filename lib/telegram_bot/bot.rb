da10e201d0dcab83ff315ce642b0aa91ec3cc711
José Tomás Albornoz <jojo@eljojo.net>
2015-07-01 00:13:11 +0200

    Added a logger and allowing to customize through options

--- lib/telegram_bot/bot.rb
+++ lib/telegram_bot/bot.rb
@@ -2,45 +2,66 @@ module TelegramBot
   class Bot
     ENDPOINT = 'https://api.telegram.org/'
 
-    attr_reader :me
-    alias_method :identity, :me
+    def initialize(opts = {})
+      # compatibility with just passing a token
+      if opts.is_a?(String)
+        opts = { token: opts }
+      end
 
-    def initialize(token)
-      @token = token
-      @timeout = 50
-      @offset = 0
+      @token = opts.fetch(:token)
+      @timeout = opts[:timeout] || 50
+      @offset = opts[:offset] || 0
+      @logger = opts[:logger] || NullLoger.new
       @connection = Excon.new(ENDPOINT, persistent: true)
-      @me = get_me
     end
 
     def get_me
-      response = request(:getMe)
-      User.new(response.result)
+      @me ||= begin
+                response = request(:getMe)
+                User.new(response.result)
+              end
     end
+    alias_method :me, :get_me
+    alias_method :identity, :me
 
-    def get_updates(&block)
+    def get_updates(opts = {}, &block)
+      return get_last_messages(opts) unless block_given?
+
+      logger.info "starting get_updates loop"
       loop do
-        response = request(:getUpdates, offset: @offset, timeout: @timeout)
-        response.result.each do |raw_update|
-          update = Update.new(raw_update)
-          @offset = update.id + 1
-          yield update.message
+        messages = get_last_messages(opts)
+        messages.each do |message|
+          logger.info "message from @#{message.chat.friendly_name}: #{message.text.inspect}"
+          yield message
         end
       end
     end
 
     def send_message(out_message)
       response = request(:sendMessage, out_message.to_h)
+      logger.info "sending message: #{out_message.text.inspect} to #{out_message.chat_friendly_name}"
       Message.new(response.result)
     end
 
-
     private
     attr_reader :token
+    attr_reader :logger
+
     def request(action, query = {})
       path = "/bot#{@token}/#{action}"
       res = @connection.post(path: path, query: query)
       ApiResponse.new(res)
+    end
+
+    def get_last_updates(opts = {})
+      response = request(:getUpdates, offset: opts[:offset] || @offset, timeout: opts[:timeout] || @timeout)
+      updates = response.result.map{|raw_update| Update.new(raw_update) }
+      @offset = updates.last.id + 1 if updates.any?
+      updates
+    end
+
+    def get_last_messages(opts = {})
+      get_last_updates(opts).map(&:message)
     end
   end
 end
---
+++ lib/telegram_bot/null_logger.rb
@@ -0,0 +1,11 @@
+require 'logger'
+
+module TelegramBot
+  class NullLoger < Logger
+    def initialize(*args)
+    end
+
+    def add(*args, &block)
+    end
+  end
+end
