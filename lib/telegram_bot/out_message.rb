014c885c1c986f92e0a96f81d916e06c24759d08
Lucas Krämer de Sousa <lucas.kramer@resultadosdigitais.com.br>
2015-10-05 03:31:24 -0300

    Bot can reply users

--- lib/telegram_bot/out_message.rb
+++ lib/telegram_bot/out_message.rb
@@ -3,6 +3,7 @@ module TelegramBot
     include Virtus.model
     attribute :chat, Channel
     attribute :text, String
+    attribute :reply_to, Message
 
     def send_with(bot)
       bot.send_message(self)
@@ -13,10 +14,18 @@ module TelegramBot
     end
 
     def to_h
-      {
-        text: text,
-        chat_id: chat.id
-      }
+      if reply_to.nil? then
+        {
+          text: text,
+          chat_id: chat.id
+        }
+      else
+        {
+          text: text,
+          chat_id: chat.id,
+          reply_to_message_id: reply_to.id
+        }
+      end
     end
   end
 end
