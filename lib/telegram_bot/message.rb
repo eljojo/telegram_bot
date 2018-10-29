deb72b0b957bf6728d7276f88e77cdbd342f7ed3
Ruben Ignacio <ruben.ignaciogz@gmail.com>
2018-10-29 18:56:09 -0500

    add new attibutes to Message

--- lib/telegram_bot/message.rb
+++ lib/telegram_bot/message.rb
@@ -6,11 +6,32 @@ module TelegramBot
     alias_method :to_i, :id
     attribute :from, User
     alias_method :user, :from
-    attribute :text, String
     attribute :date, DateTime
     attribute :chat, Channel
+    attribute :forward_from, User
+    attribute :forward_from_chat, Channel
+    attribute :forward_from_message_id, Integer
+    attribute :forward_signature, String
+    attribute :forward_date, DateTime
     attribute :reply_to_message, Message
+    attribute :edit_date, DateTime
+    attribute :media_group_id, String
+    attribute :author_signature, String
+    attribute :text, String
     attribute :entities, Array[MessageEntity]
+    attribute :caption_entities, Array[MessageEntity]
+    attribute :caption, String
+    attribute :new_chat_members, Array[User]
+    attribute :left_chat_member, User
+    attribute :new_chat_title, String
+    attribute :delete_chat_photo, Boolean, default: false
+    attribute :group_chat_created, Boolean, default: false
+    attribute :supergroup_chat_created, Boolean, default: false
+    attribute :channel_chat_created, Boolean, default: false
+    attribute :migrate_to_chat_id, Integer
+    attribute :migrate_from_chat_id, Integer
+    attribute :pinned_message, Message
+    attribute :connected_website, String
 
     def reply(&block)
       reply = OutMessage.new(chat: chat)
