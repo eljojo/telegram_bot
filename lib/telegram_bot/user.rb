2f9f5fa0bf76f845f95139f3e599612706c0247d
Ruben Ignacio <ruben.ignaciogz@gmail.com>
2018-10-31 04:05:21 -0500

    add full_name method

--- lib/telegram_bot/user.rb
+++ lib/telegram_bot/user.rb
@@ -6,11 +6,15 @@ module TelegramBot
     attribute :first_name, String
     attribute :last_name, String
     attribute :username, String
-    attribute :is_bot, Boolean, default_value: false
+    attribute :is_bot, Boolean
     attribute :language_code, String
 
     def ==(other)
       other.is_a?(self.class) && other.hash == hash
+    end
+
+    def full_name
+      @last_name ? "#{@first_name} #{@last_name}" : @first_name
     end
 
     def hash
