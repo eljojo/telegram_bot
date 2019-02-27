e37d6bd1afa2dec48ab7c9eadc04d24b741c24c7
Ruben Ignacio <ruben.ignaciogz@gmail.com>
2019-02-27 08:46:46 -0500

    added new attributes to Chat

--- lib/telegram_bot.rb
+++ lib/telegram_bot.rb
@@ -2,26 +2,28 @@ require 'excon'
 require 'virtus'
 require 'json'
 
-require "telegram_bot/version"
-require "telegram_bot/result"
-require "telegram_bot/null_logger"
-require "telegram_bot/user"
-require "telegram_bot/group_chat"
-require "telegram_bot/chat"
-require "telegram_bot/message_entity"
-require "telegram_bot/message"
-require "telegram_bot/keyboard"
-require "telegram_bot/reply_keyboard_hide"
-require "telegram_bot/reply_keyboard_markup"
-require "telegram_bot/force_replay"
-require "telegram_bot/out_message"
-require "telegram_bot/update"
-require "telegram_bot/api_response"
-require "telegram_bot/bot"
-require "telegram_bot/connection"
-
-
 module TelegramBot
+  {
+    Version: "version",
+    Result: "result",
+    NullLogger: "null_logger",
+    User: "user",
+    Chat: "chat",
+    MessageEntity: "message_entity",
+    Message: "message",
+    Keyboard: "keyboard",
+    ReplyKeyboardHide: "reply_keyboard_hide",
+    ReplyKeyboardMarkup: "reply_keyboard_markup",
+    ForceReplay: "force_replay",
+    OutMessage: "out_message",
+    Update: "update",
+    ApiResponse: "api_response",
+    Bot: "bot",
+    Connection: "connection",
+  }.each do |key, val|
+    autoload(key, "telegram_bot/#{val}")
+  end
+
   def self.new(opts)
     Bot.new(opts)
   end
--- lib/telegram_bot/chat.rb
+++ lib/telegram_bot/chat.rb
@@ -2,8 +2,17 @@ module TelegramBot
   class Chat
     include Virtus.model
     attribute :id, Integer
-    attribute :username, String
+    attribute :type, String
     attribute :title, String
+    attribute :username, String
+    attribute :first_name, String
+    attribute :last_name, String
+    attribute :all_members_are_administrators, Boolean
+    attribute :description, String
+    attribute :invite_link, String
+    attribute :pinned_message, Message
+    attribute :sticker_set_name, String
+    attribute :can_set_sticker_set, Boolean
 
     def friendly_name
       username ? "@#{username}" : "chat #{title.inspect}"
